7c075c497f24bbe389ab2a903f36667ba31e7b28
Suren Nihalani <suren.k.n@icloud.com>
2014-01-05 10:53:25 +0530

    Cleaning up dest.php

--- dest.php
+++ dest.php
@@ -21,29 +21,31 @@ function last($array, $key) {
  * The return value is ignored.
  */
 function dest_ParseInput( $plugin_id ) {
+	$_SESSION['refresh'] = 0;
+	if(!isset($_POST["submit_start_end"] )) {
+		return;
+	}
 
-	if(isset($_POST["submit_start_end"] )) {
-
-		print '
-			<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>	
-			<script src="http://code.highcharts.com/highcharts.js"></script>
-			<script src="http://code.highcharts.com/modules/exporting.js"></script>';
-		$opts = array();
-		$opts['start'] = $_POST['start'];
-		$opts['end'] = $_POST['end'];
-		$out_list = nfsend_query("dest::create_graph", $opts);
-		$start_date = DateTime::createFromFormat('Y-m-d|', $_POST['start']);
-		$end_date = DateTime::createFromFormat('Y-m-d|', $_POST['end']);
-		$end_date->add(new DateInterval("P1D"));
+	print '
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>	
+		<script src="http://code.highcharts.com/highcharts.js"></script>
+		<script src="http://code.highcharts.com/modules/exporting.js"></script>';
+	$opts = array();
+	$opts['start'] = $_POST['start'];
+	$opts['end'] = $_POST['end'];
+	$out_list = nfsend_query("dest::create_graph", $opts);
+	$start_date = DateTime::createFromFormat('Y-m-d|', $_POST['start']);
+	$end_date = DateTime::createFromFormat('Y-m-d|', $_POST['end']);
+	$end_date->add(new DateInterval("P1D"));
 	
-		$all_dates = new DatePeriod( $start_date, new DateInterval('P1D'), $end_date );
-		$dates_in_str = array();
-		foreach ($all_dates as $a_date) {
-			array_push($dates_in_str, str_replace(' ','',"'". $a_date->format('Y-m-d'))."'");
-		}
-		echo "
-			<script>
-			$(function () {
+	$all_dates = new DatePeriod( $start_date, new DateInterval('P1D'), $end_date );
+	$dates_in_str = array();
+	foreach ($all_dates as $a_date) {
+		array_push($dates_in_str, str_replace(' ','',"'". $a_date->format('Y-m-d'))."'");
+	}
+	echo "
+	<script>
+		$(function () {
 			$('#dest_container').highcharts({
 			chart: {
 				type: 'area'
@@ -55,17 +57,17 @@ function dest_ParseInput( $plugin_id ) {
 			},
 			xAxis: {
 				categories: 	
-		";
-		echo "[". join(', ', $dates_in_str) . "]";
-		echo ", tickmarkPlacement: 'on',
-                title: {
-                    enabled: false
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Megabytes'
-                },
+	";
+	echo "[". join(', ', $dates_in_str) . "]";
+	echo ", tickmarkPlacement: 'on',
+            title: {
+              enabled: false
+            }
+          },
+          yAxis: {
+            title: {
+             text: 'Megabytes'
+          },
                 labels: {
                     formatter: function() {
                         return this.value / 1000000;
@@ -87,24 +89,20 @@ function dest_ParseInput( $plugin_id ) {
                     }
                 }
             },";	
-		echo "series: [";
-		foreach ($out_list as $key => $value) {
-			echo "{ name: '". $key . "', data: [";
-			echo join(", " , $value);
-			echo ']}';
-			if(!last($out_list, $key)) {
-				echo ',';
-			}
+	echo "series: [";
+	foreach ($out_list as $key => $value) {
+		echo "{ name: '". $key . "', data: [";
+		echo join(", " , $value);
+		echo ']}';
+		if(!last($out_list, $key)) {
+			echo ',';
 		}
-		echo "]";
-
-		echo "        });
-	    });</script>";
-		SetMessage('info', "POST is set");
-	} else {
-		SetMessage('info', "POST is not set");
 	}
+	echo "]";
 
+	echo "        });
+    });</script>";
+	SetMessage('info', "Query succeeded!");
 } // End of dest_ParseInput
 
 
@@ -115,14 +113,25 @@ function dest_ParseInput( $plugin_id ) {
  * Its return value is ignored.
  */
 function dest_Run( $plugin_id ) {
+	$start_date = '2014-01-02';
+	$end_date = '2014-01-03';
+	if(isset($_POST["submit_start_end"] )) {
+		$start_date = $_POST['start'];
+		$end_date = $_POST['end'];
+	} 
 	echo '
 		<form method="post" action="/nfsen/nfsen.php"  >
-		Start date: <input type="date" name="start" value="2014-01-02"/>
-		End date: <input type="date" name="end" value="2014-01-03"  />
+		Start date: <input type="date" name="start" value="' 
+		. $start_date .	
+	'"/>
+	End date: <input type="date" name="end" value="' 
+	. $end_date .
+	'"  />
 		<input type="submit" name="submit_start_end" />
 		</form>
 		';
 	echo "<div id=\"dest_container\" style=\"min-width: 310px; height: 400px; margin: 0 auto\"></div>";
+
 
 } // End of dest_Run
 
